e9319895f74e823857965d52caf43b2c938c4f3d
yelamery <yasminel-amery@Yasmins-MacBook-Pro.local>
2017-01-30 20:40:14 +0000

    scroll and sticky nav

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log('it works');
+
+$(document).ready(function(){
+	$('nav a').on('click', function(e){
+  	e.preventDefault();
+  	var target = $(this).attr('href');
+  	var scrollPosition = $(target).offset().top;
+  	$('html, body').animate({
+  	  scrollTop: scrollPosition
+  	}, 1000);
+	});
+});
