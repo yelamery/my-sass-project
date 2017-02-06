dd99837812ad5132fbc760727862dd1ce9cde6a9
yelamery <yasminel-amery@Yasmins-MacBook-Pro.local>
2017-02-06 14:39:26 +0000

    arrow project page

--- js/main.js
+++ js/main.js
@@ -9,4 +9,12 @@ $(document).ready(function(){
   	  scrollTop: scrollPosition
   	}, 1000);
 	});
+	$('.arrow-box a').on('click', function(e){
+  	e.preventDefault();
+  	var target = $(this).attr('href');
+  	var scrollPosition = $(target).offset().top;
+  	$('html, body').animate({
+  	  scrollTop: scrollPosition
+  	}, 1000);
+	});
 });
